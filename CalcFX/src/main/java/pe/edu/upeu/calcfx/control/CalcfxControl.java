1a37412703193f6e00cbb50319e99eb04e617c2b
EdgarKbana <nathanmillerdrake@gmail.com>
2025-04-15 22:48:27 -0500

    Add files via upload

--- CalcFX/src/main/java/pe/edu/upeu/calcfx/control/CalcfxControl.java
+++ CalcFX/src/main/java/pe/edu/upeu/calcfx/control/CalcfxControl.java
@@ -49,6 +49,33 @@ public class CalcfxControl {
                         return;
                     }
                     break;
+                case "^": // Suponiendo que "^" se usa para la potencia
+                    resultado = Math.pow(num1, num2);
+                    break;
+                case "√": // Suponiendo que "sqrt" se usa para la raíz cuadrada, sino calcula poner el numero primero luego la raiz y para que resuelva poner un numero extra al final
+                    if (num1 >= 0) {
+                        resultado = Math.sqrt(num1);
+                    } else {
+                        txtResultado.setText("Error: Raíz de número negativo");
+                        return;
+                    }
+                    break;
+                case "1/": // Suponiendo que "1/" se usa para la inversa
+                    // Para la inversa, el segundo número (num2) no se utiliza directamente en esta lógica
+                    if (num1 != 0) {
+                        resultado = 1.0 / num1;
+                    } else {
+                        txtResultado.setText("Error: División por cero");
+                        return;
+                    }
+                    break;
+                case "π": // Suponiendo que "pi" se usa para obtener el valor de Pi igual poner un numero al final para calcular
+                    resultado = Math.PI;
+                    txtResultado.setText(String.valueOf(resultado));
+                    return; // Salimos del switch después de mostrar Pi
+                default:
+                    txtResultado.setText("Operador inválido");
+                    return;
             }
 
             txtResultado.setText(String.valueOf(resultado));
@@ -64,7 +91,7 @@ public class CalcfxControl {
         Button boton = (Button) event.getSource();
         switch (boton.getId()) {
             case "btn0", "btn1", "btn2", "btn3", "btn4", "btn5", "btn6", "btn7", "btn8", "btn9": {escribirNumero(boton.getText());} break;
-            case "btnSum", "btnDiv", "btnRest", "btnMulti":{ agregarOperador(boton.getText());} break;
+            case "btnSum", "btnDiv", "btnRest", "btnMulti", "btnPot", "btnRaiz", "btnUno", "btnPi" :{ agregarOperador(boton.getText());} break;
             case "btnBorrar":{txtResultado.setText("");} break;
             case "btnIgual":{calcularResultado();} break;
 
